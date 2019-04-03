8bb5cb4d9953014ec9ff15b2a7389380ef4c8aaf
Henri Geneste <Henri@unknown001f5bb48482.gateway.2wire.net>
2019-04-02 18:11:11 -0700

    done

--- Homework1_Henri_Geneste_1675311.py
+++ Homework1_Henri_Geneste_1675311.py
@@ -2,43 +2,33 @@ from __future__ import print_function, absolute_import, division, unicode_litera
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-import random as random
-#Inputs a data array of any size (type float)
+
 def makeArray(size):
-	#Create Array with proper size and type
-	pArr = np.empty(size, dtype = float)
 	
-	mean_rms = False
-	while not mean_rms:
-		total = 0.0
-		totalrms = 0.0
-		
-		#Populate Array
-		for i in range(0,size):
-			pArr[i] = random.uniform(-2,4)
-			total = total + pArr[i]
-			totalrms = totalrms + pArr[i]**2
-		
-		#Check for proper mean and RMS values (rounded)
-		print('Mean is: ', round(total/size))
-		print('RMS is: ', round(np.sqrt(totalrms/size)))
-		if round((total/size)) == 1.0:
-			if round(np.sqrt(totalrms/size)) == 2.0:
-				mean_rms = True
-			else:
-				mean_rms = False
+	#Create Array with proper size, mean, and RMS:
+	pArr = np.random.normal(1,2,size)
+	
 	return pArr
 
-#Plots the array as a histogram with an inputted bin size
 def plotArr(pArr, binSize):
-	binNum = int(6/binSize)
-	plt.hist(pArr, bins = binNum, range = (-2,4))
+	
+	#plot the histogram of the randomly generated data
+	count, bins, ignored = plt.hist(pArr, binSize, density = True, color = 'purple')
+
+	#Plot the associated Gaussian distribution
+	plt.plot(bins, 1/(2 * np.sqrt(2 * np.pi)) * np.exp( - (bins - 1)**2 / (2 * 2**2) ), linewidth = 2, color = 'green')
+	
+	#Display graph/save figure as PDF
 	plt.show()
+	plt.savefig("Assignment1_ASTR136_Henri_Geneste_1675311_Plot.pdf")
 	return
-#Overplots the Gaussian that best describes the data
 
-#Generates a postscript/PDF file of the plot
-
+#Create the array:
 myArr = makeArray(1000)
-print(myArr)
-plotArr(myArr,0.5)
+
+#Check mean and RMS of array:
+print('The mean is: ', np.mean(myArr))
+print('The RMS is: ', np.std(myArr))
+
+#Plot random array using a specified number of bins (in this case I chose 10)
+plotArr(myArr,10)
